8851d97a94e4c043f36b5c725cc9a121d2e97382
xiaozhen <xiaozhen@ccplaying.com>
2016-03-10 15:12:05 +0800

    init

--- testGit1.sh
+++ testGit1.sh
@@ -1,6 +1,2 @@
-#!/bin/sh
-TARGET=$1
-sh ./version.sh
-cocos compile -p ios --lua-encrypt --lua-encrypt-key "d1_key_" --lua-encrypt-sign "d1_sign_" --compile-script 1 -t $TARGET -m release --sign-identity "iPhone Distribution: Cao Jun (977549GP8P)"
+file 1
 
-2-test-1
